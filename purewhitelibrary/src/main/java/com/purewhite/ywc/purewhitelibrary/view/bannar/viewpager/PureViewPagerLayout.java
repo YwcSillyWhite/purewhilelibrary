585754548512c55919e406b64df0a55fddd1f4f6
yuwenchao <1075770029@qq.com>
2019-09-16 11:00:47 +0800

    初代bannar，优化完成

--- purewhitelibrary/src/main/java/com/purewhite/ywc/purewhitelibrary/view/bannar/viewpager/PureViewPagerLayout.java
+++ purewhitelibrary/src/main/java/com/purewhite/ywc/purewhitelibrary/view/bannar/viewpager/PureViewPagerLayout.java
@@ -8,13 +8,17 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.purewhite.ywc.purewhitelibrary.R;
+import com.purewhite.ywc.purewhitelibrary.config.SizeUtils;
 import com.purewhite.ywc.purewhitelibrary.view.bannar.viewpager.listener.OnPureChangeListener;
-import com.purewhite.ywc.purewhitelibrary.window.utils.WindowPureUtils;
 
+import io.reactivex.annotations.NonNull;
+
+/**
+ * 带下标的banner
+ */
 public class PureViewPagerLayout extends RelativeLayout {
 
     public PureViewPager pureViewPager;
-    private LayoutParams paramsViewPager, paramsLinear;
     private LinearLayout.LayoutParams paramsView;
     private LinearLayout linearLayout;
     private int cricleDraw = R.drawable.pure_selector_cricle;
@@ -33,24 +37,23 @@ public class PureViewPagerLayout extends RelativeLayout {
     }
 
     private void initView() {
+        //viewpager
         pureViewPager = new PureViewPager(getContext());
-        paramsViewPager = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        addView(pureViewPager, paramsViewPager);
+        setParamsViewPager(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        addView(pureViewPager);
 
+
+        //linearLayout
         linearLayout = new LinearLayout(getContext());
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        paramsLinear = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LayoutParams paramsLinear = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         paramsLinear.addRule(ALIGN_PARENT_BOTTOM);
-        paramsLinear.bottomMargin=5;
-        addView(linearLayout, paramsLinear);
+        paramsLinear.addRule(CENTER_HORIZONTAL);
+        paramsLinear.bottomMargin= SizeUtils.dip2px(10);
+        setParamsLinearLayout(paramsLinear,LinearLayout.HORIZONTAL);
+        addView(linearLayout);
 
 
-
-        paramsView=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        paramsView.width=50;
-        paramsView.height=50;
-        paramsView.rightMargin=5;
-        paramsView.leftMargin=5;
+        setParamsLinearView(SizeUtils.dip2px(15),SizeUtils.dip2px(4),SizeUtils.dip2px(4),0,0);
 
 
         pureViewPager.setOnPureChangeListener(new OnPureChangeListener() {
@@ -78,7 +81,53 @@ public class PureViewPagerLayout extends RelativeLayout {
     }
 
 
-    public void setLayoutView(int size,int selectorPosition) {
+    public PureViewPagerLayout setCricleDraw(int cricleDraw) {
+        this.cricleDraw = cricleDraw;
+        return this;
+    }
+
+    /**
+     * 设置viewpager
+     * @param paramsView
+     */
+    public PureViewPagerLayout setParamsViewPager(@NonNull ViewGroup.LayoutParams paramsView) {
+        pureViewPager.setLayoutParams(paramsView);
+        return this;
+    }
+
+
+    /**
+     * 设置LinearLayout
+     * @param paramsView
+     */
+    public PureViewPagerLayout setParamsLinearLayout(@NonNull ViewGroup.LayoutParams paramsView,int orientation) {
+        linearLayout.setLayoutParams(paramsView);
+        linearLayout.setOrientation(orientation);
+        return this;
+    }
+
+
+    /**
+     * 设置下标view
+     * @param diameter 直接
+     * @param left   左
+     * @param right  右
+     * @param top    上
+     * @param bottom 下
+     */
+    public PureViewPagerLayout setParamsLinearView(int diameter,int left,int right,int top,int bottom) {
+        paramsView=new LinearLayout.LayoutParams(diameter, diameter);
+        paramsView.leftMargin=left;
+        paramsView.rightMargin=right;
+        paramsView.topMargin=top;
+        paramsView.bottomMargin=bottom;
+        return this;
+    }
+
+
+
+    //设置下标布局
+    public PureViewPagerLayout setLayoutView(int size,int selectorPosition) {
         linearLayout.removeAllViews();
         if (size>1)
         {
@@ -90,7 +139,7 @@ public class PureViewPagerLayout extends RelativeLayout {
                 linearLayout.addView(view,paramsView);
             }
         }
-
+        return this;
     }
 
 
